c36d21bfb26385c3266ff8e657da9bf85e0908b2
Daniele Alessandri <suppakilla@gmail.com>
2011-05-16 22:01:35 +0200

    Do not override the __toString() magic method of exceptions.

--- lib/Predis/ServerException.php
+++ lib/Predis/ServerException.php
@@ -3,10 +3,6 @@
 namespace Predis;
 
 class ServerException extends PredisException implements IReplyObject {
-    public function __toString() {
-        return $this->getMessage();
-    }
-
     public function getRedisErrorType() {
         $message = $this->getMessage();
         return substr($message, 0, strpos($message, ' '));
